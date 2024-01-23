8ef8dad370a326fcbc67f55cde8c55c10dd01ecc
Gyumin Lee <gyumin.q.lee@gmail.com>
2024-01-23 23:58:20 +0900

    240123 [Course1] Firebase 세팅하기

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import "index.css";
 import App from "App";
 import reportWebVitals from "reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import firebase from "firebaseApp";
+
+console.log(firebase);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
