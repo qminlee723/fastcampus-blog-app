f6793892efb0a81c9a376582ce5bc0338202fae3
Gyumin Lee <gyumin.q.lee@gmail.com>
2024-01-24 00:30:37 +0900

    240124 [Course1] Firebase Auth 세팅하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import { app } from "firebaseApp";
+import { getAuth } from "firebase/auth";
 import Router from "components/Router";
 
 function App() {
-  return <Router />;
+  const auth = getAuth(app); // app을 넣어줘야 동작함
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+  return <Router isAuthenticated={isAuthenticated} />;
 }
 
 export default App;
--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,4 +1,7 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, FirebaseApp, getApp } from "firebase/app";
+import "firebase/auth";
+
+export let app: FirebaseApp;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -9,6 +12,13 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_ID,
 };
+
+// 처음 앱을 가져올 때 앱이 initialize되었다면 기존에 초기화된 앱을 가져오고, 그게 아니면 초기화하기
+try {
+  app = getApp("app");
+} catch (e) {
+  app = initializeApp(firebaseConfig, "app");
+}
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
