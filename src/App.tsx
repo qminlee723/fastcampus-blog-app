e5e1e7ce2acda747d5473a41a9f5a5d3039ca9a8
Gyumin Lee <gyumin.q.lee@gmail.com>
2024-01-25 23:57:16 +0900

    240125 Firebase Auth login

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { app } from "firebaseApp";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Router from "components/Router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Loader from "components/Loader";
 
 function App() {
   const auth = getAuth(app); // app을 넣어줘야 동작함
+
+  // auth를 체크하기 전에 (initalize전) 에는 loader를 띄워주는 용도
+  const [init, setInit] = useState<boolean>(false);
+  // auth의 currentUser가 있으면 authenticated로 변경
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
   return (
     <>
       <ToastContainer />
-      <Router isAuthenticated={isAuthenticated} />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </>
   );
 }
