9facd6a4eed11ef48e803264f795ea5d1413f6ed
Gyumin Lee <gyumin.q.lee@gmail.com>
2024-02-12 23:55:44 +0900

    240212 [Course1] 댓글 UI 작업

---
+++ src/components/Comments.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const COMMENTS = [];
+export default function Comments() {
+  const [comment, setComment] = useState("");
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "comment") {
+      setComment(value);
+    }
+  };
+
+  return (
+    <div className="comments">
+      <form className="comments__form">
+        <div className="form__block">
+          <label htmlFor="comment">댓글 입력</label>
+          <textarea
+            name="comment"
+            id="comment"
+            required
+            value={comment}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form__block">
+          <input type="submit" value="입력" className="form__btn-submit" />
+        </div>
+      </form>
+      <div className="comments__list">
+        {COMMENTS?.map((comment) => (
+          <div key={comment.id} className="comment__box">
+            <div className="comment__profile-box">
+              <div className="comment__email">{comment?.email}</div>
+              <div className="comment__date">{comment?.date}</div>
+              <div className="comment_delete">삭제</div>
+            </div>
+            <div className="comment__text">{comment?.content}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
